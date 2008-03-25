253e0f1aa0666932357a845a7e4df96dd11244dc
miljenko <miljenko@a13bc769-3149-0410-86b7-5ddceca669d6>
2008-03-25 19:52:38 +0000

    Added count_records.py and modified delete_records.py to dynamically delete from all tables

--- delete_records.py
+++ delete_records.py
@@ -3,12 +3,13 @@ from pysqlite2 import dbapi2 as sqlite
 con = sqlite.connect('agcmpl.db')
 cur = con.cursor()
 
-cur.execute('DELETE FROM player')
-cur.execute('DELETE FROM season')
-cur.execute('DELETE FROM round')
-cur.execute('DELETE FROM game')
-cur.execute('DELETE FROM prediction')
-cur.execute('DELETE FROM team')
-cur.execute('DELETE FROM country')
+cur.execute("""SELECT 'DELETE FROM ' || name || ' WHERE 1 = 1', name
+                 FROM sqlite_master
+                WHERE type = 'table'
+                ORDER BY 2""")
+
+for delete_statement in cur.fetchall():
+  count = cur.execute(delete_statement[0]).rowcount
+  print 'Deleted %6d records from %-15s' % (count, delete_statement[1])
 
 con.commit()
